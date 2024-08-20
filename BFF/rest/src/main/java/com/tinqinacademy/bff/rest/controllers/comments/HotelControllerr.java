460ce2fa5cd15741a829cb264e44a5a415c4216c
unknown <rostyslavz19@gmail.com>
2024-08-20 10:37:24 +0300

    [BFF-9] Adding MyHotel Controller

--- BFF/rest/src/main/java/com/tinqinacademy/bff/rest/controllers/comments/HotelControllerr.java
+++ BFF/rest/src/main/java/com/tinqinacademy/bff/rest/controllers/comments/HotelControllerr.java
@@ -4,7 +4,6 @@ import com.tinqinacademy.bff.api.operations.comments.leavescommentsforcertainroo
 import com.tinqinacademy.bff.api.operations.comments.leavescommentsforcertainroom.LeaveCommentRequest;
 import com.tinqinacademy.bff.rest.controllers.base.BaseController;
 import com.tinqinacademy.comments.api.operations.editcommentcontentbyuser.EditCommentContentInput;
-import com.tinqinacademy.comments.api.operations.leavescommentsforcertainroom.LeaveCommentInput;
 import com.tinqinacademy.comments.restexport.CommentsClient;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Comments Client hotel - REST APIs")
-public class HotelController extends BaseController {
+public class HotelControllerr extends BaseController {
 
     private final LeaveCommentOperation leaveCommentOperation;
     private final CommentsClient commentsClient;
